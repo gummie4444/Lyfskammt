25f0e0a8a88f5f86f43a4242c1548a55720ba052
skossi <thorsteinn91@gmail.com>
2014-08-06 15:09:25 +0000

    nokkur plugins

---
+++ public/js/directives/mobiscroll-dir.js
@@ -0,0 +1,32 @@
+angular.module('mobiscroll-dir', [])
+
+.directive("mobiscroll", function() {
+  return {
+    restrict: 'A',
+    require: 'ngModel',
+    scope: {
+      scroller : '=mobiscroll',
+      update : '=update',
+      model : '=ngModel'
+    },
+    link: function (scope, elm, attrs, ctlr, ngModel) {
+        elm.mobiscroll().time({
+                theme: scope.scroller.theme,
+                display: scope.scroller.display,
+                mode: scope.scroller.mode,
+                minWidth: scope.scroller.minWidth,
+                height: scope.scroller.height,
+                timeFormat: scope.scroller.timeFormat,
+                timeWheels: scope.scroller.timeWheels,
+                layout: scope.scroller.layout,
+                // onChange: function() {
+                //     scope.update();
+                // }
+            });
+
+        scope.$watch("model", function(value) {
+                elm.mobiscroll('setValue',value);
+            });
+    }
+  };
+});
